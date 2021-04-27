a7cf4333a2567c8cc7be383d97be9c8c7f06dc22
ruddy-castro <60947941+ruddy-castro@users.noreply.github.com>
2021-04-26 23:34:21 -0700

    Update client.py
    Added constants.

--- Peer to Peer Network Assignment/client.py
+++ Peer to Peer Network Assignment/client.py
@@ -2,7 +2,12 @@ import socket
 import threading
 import sys
 import time
-from Server_Client_Code.constants import *
+
+BYTE_SIZE = 1024
+HOST = '127.0.0.1'
+PORT = 5000
+PEER_BYTE_DIFFERENTIATOR = b'\x11' 
+REQUEST_STRING = "req"
 
 class Client:
     # Constructor for client
