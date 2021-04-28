45edb72cccbe4e4ab7a2ccb014a154f227d67087
David H Kwon <55861630+davidkwonh@users.noreply.github.com>
2021-04-28 15:35:14 -0700

    Update P2P.py

--- Peer to Peer Network Assignment/P2P.py
+++ Peer to Peer Network Assignment/P2P.py
@@ -7,6 +7,10 @@ directpath = os.getcwd()
 prevdir = directpath + "/read_file.txt"
 newdir = directpath + "/new_file.txt"
 
+class ip:
+    # make ourself the default peer
+    address = ['127.0.0.1']
+
 def prepfile(dir=prevdir):
     read_data = None
     with open(dir, 'r') as file:
@@ -24,6 +28,31 @@ def makefile(encrypted_file):
 def main():
     targetfile = prepfile()
     makefile(targetfile)
+
+    msg = fileIO.convert_to_bytes()
+    while True:
+        try:
+            print("Establishing Connection")
+            # sleep a random time between 1 -5 seconds
+            time.sleep(randint(RAND_TIME_START, RAND_TIME_END))
+            for ipaddy in ip.address:
+                try:
+                    client = Client(ipaddy)
+                except KeyboardInterrupt:
+                    sys.exit(0)
+                except:
+                    pass
+
+                # become the server
+                try:
+                    server = Server(msg)
+                except KeyboardInterrupt:
+                    sys.exit()
+                except:
+                    pass
+
+        except KeyboardInterrupt as e:
+            sys.exit(0)
 
 def test():
     print(socket.gethostname())
