5e16d91db129376af006e794aab5fea3d3dad431
David H Kwon <55861630+davidkwonh@users.noreply.github.com>
2021-04-28 15:27:39 -0700

    Update P2P.py

--- Peer to Peer Network Assignment/P2P.py
+++ Peer to Peer Network Assignment/P2P.py
@@ -1,16 +1,36 @@
 import os
 import socket
+from client import *
+from server import *
+
+directpath = os.getcwd()
+prevdir = directpath + "/read_file.txt"
+newdir = directpath + "/new_file.txt"
+
+def prepfile(dir=prevdir):
+    read_data = None
+    with open(dir, 'r') as file:
+        read_data = file.read()
+    return read_data.encode("utf-8")
+
+def makefile(encrypted_file):
+    encrypted_file = data.decode("utf-8")
+    print("Writing to file")
+    with open(newdir, 'w') as file:
+        file.write(encrypted_file)
+    return True
+
+
+def main():
+    targetfile = prepfile()
+    makefile(targetfile)
 
 def test():
     print(socket.gethostname())
-    print()
-
     devices = []
-
     for device in os.popen('arp -a'): 
         print(device)
 
-        
 def main():
     test()
 
